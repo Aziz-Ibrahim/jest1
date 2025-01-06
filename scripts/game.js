6a2c448cd8789ddcff586036251ae1eee347e8e2
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-01-06 00:15:36 +0000

    Add spy test and add player turn function

--- scripts/game.js
+++ scripts/game.js
@@ -15,8 +15,8 @@ function newGame() {
         if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
                 let move = e.target.getAttribute("id");
-                lightsOn(move);
                 game.playerMoves.push(move);
+                lightsOn(move);
                 playerTurn();
             });
             circle.setAttribute("data-listener", "true");
@@ -50,6 +50,20 @@ function lightsOn(circ) {
     }, 400);
 }
 
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
@@ -60,5 +74,6 @@ module.exports = {
     showScore,
     addTurn,
     lightsOn,
-    showTurns
+    showTurns,
+    playerTurn
 };
